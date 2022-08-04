419e3eae7e3ffabd003c6b70cafafb522bf19fc2
Ilker Sener <eilkersener@gmail.com>
2022-08-04 18:33:38 +0300

    static files

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
-
+const path = require('path');
 const port = 3000;
+const ejs = require('ejs');
+
+app.set("view engine","ejs");
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.render('index')
+});
 
 app.listen(port, () => {
   console.log(`sunucu ${port} portunda başlatıldı`);
